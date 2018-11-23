653f10fd31770de8b0eedc01dc433f7adb75adb5
AAVN\pntan <tan.pham@axonactive.com>
2018-11-23 16:22:04 +0700

    Adding main class in CDI 3

---
+++ cdi-qualifiers/src/main/java/com/axonactive/cdi/demo/main/MainApplication.java
@@ -0,0 +1,23 @@
+package com.axonactive.cdi.demo.main;
+
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+
+import com.axonactive.cdi.demo.model.Checkout;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		try (WeldContainer container = new Weld().initialize()) {
+			Checkout checkout = container.select(Checkout.class).get();
+			// This line equals 
+			/*
+			 * @Inject
+				private Checkout checkout;
+			 */
+
+			checkout.finishCheckout();
+		}
+	}
+
+}
---
+++ cdi-qualifiers/src/main/java/com/axonactive/cdi/demo/model/Checkout.java
@@ -0,0 +1,7 @@
+package com.axonactive.cdi.demo.model;
+
+public class Checkout {
+	public void finishCheckout() {
+		System.out.println("Finishing Checkout");
+	}
+}
