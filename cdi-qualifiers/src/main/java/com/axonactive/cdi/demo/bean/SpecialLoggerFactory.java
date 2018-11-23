1af26af01fc1c4e52caa20a92156e31a326ed1c9
AAVN\pntan <tan.pham@axonactive.com>
2018-11-23 17:50:43 +0700

    First way to tell CDI which LogConfiguration should be used

--- cdi-qualifiers/src/main/java/com/axonactive/cdi/demo/bean/SpecialLoggerFactory.java
+++ cdi-qualifiers/src/main/java/com/axonactive/cdi/demo/bean/SpecialLoggerFactory.java
@@ -1,12 +1,14 @@
 package com.axonactive.cdi.demo.bean;
 
+import javax.enterprise.inject.Produces;
+
 import com.axonactive.cdi.demo.service.SpecialLogger;
 
 public class SpecialLoggerFactory {
 	
+	@Produces
 	public SpecialLogger createLogger() {
         LogConfiguration logInDebugMode = new LogConfiguration(true, false);
- 
         return new SpecialLogger(logInDebugMode);
     }
 	
