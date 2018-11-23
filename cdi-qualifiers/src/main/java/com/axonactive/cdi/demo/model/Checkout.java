3e1eb684c4e1baef5629859bd7385fffe0754313
AAVN\pntan <tan.pham@axonactive.com>
2018-11-23 17:30:13 +0700

    Inject SpecialLogger class to checkout function

--- cdi-qualifiers/src/main/java/com/axonactive/cdi/demo/model/Checkout.java
+++ cdi-qualifiers/src/main/java/com/axonactive/cdi/demo/model/Checkout.java
@@ -1,8 +1,12 @@
 package com.axonactive.cdi.demo.model;
 
+import javax.inject.Inject;
+
 import com.axonactive.cdi.demo.service.SpecialLogger;
 
 public class Checkout {
+	
+	@Inject
 	private SpecialLogger logger;
 	 
     public void finishCheckout() {
