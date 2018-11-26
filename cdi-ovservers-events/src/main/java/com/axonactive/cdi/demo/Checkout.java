9b8ad98e7e4961dad0869c7c5bef8052197e8b87
AAVN\pntan <tan.pham@axonactive.com>
2018-11-26 18:21:27 +0700

    cdi-events-and-observers: Refactoring to use CDI Events and Observers

--- cdi-ovservers-events/src/main/java/com/axonactive/cdi/demo/Checkout.java
+++ cdi-ovservers-events/src/main/java/com/axonactive/cdi/demo/Checkout.java
@@ -1,14 +1,18 @@
 package com.axonactive.cdi.demo;
 
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
 
 public class Checkout {
 
 	@Inject
-    private EmailSender emailSender;
- 
-    @Inject
-    private MetricCreator metrics;
+    private Event<CheckoutEvent> event;
+    
+//	@Inject
+//    private EmailSender emailSender;
+// 
+//    @Inject
+//    private MetricCreator metrics;
  
 //    @Inject
 //    private PushNotification pushNotification;
@@ -21,10 +25,10 @@ public class Checkout {
  
     public void finishCheckout(Order order) {
         System.out.println("Finishing Checkout with Id: " + order.getId());
- 
-        emailSender.sendEmailFor(order.getBuyer());
- 
-        metrics.createMetricFor(order);
+        event.fire(new CheckoutEvent(order));
+//        emailSender.sendEmailFor(order.getBuyer());
+// 
+//        metrics.createMetricFor(order);
  
 //        pushNotification.send(order);
 // 
--- cdi-ovservers-events/src/main/java/com/axonactive/cdi/demo/EmailSender.java
+++ cdi-ovservers-events/src/main/java/com/axonactive/cdi/demo/EmailSender.java
@@ -1,9 +1,13 @@
 package com.axonactive.cdi.demo;
 
+import javax.enterprise.event.Observes;
+
 public class EmailSender {
 
-	public void sendEmailFor(Buyer buyer) {
-        System.out.println("Sending email to: " + buyer.getEmail());
-    }
+	public void sendEmailFor(@Observes CheckoutEvent event) {
+		Buyer buyer = event.getOrder().getBuyer();
+
+		System.out.println("Sending email to: " + buyer.getEmail());
+	}
 
 }
